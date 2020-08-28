3abf280844d873aaf44e0bcbb4f88df1801ce54f
Tiago Pereira <pereiratiaggo@gmail.com>
2020-08-28 09:38:40 -0300

    Update dataTableOnEmpregadorWEB.user.js
    Adjusting urls and adding other filters via table id

--- dataTableOnEmpregadorWEB.user.js
+++ dataTableOnEmpregadorWEB.user.js
@@ -1,11 +1,13 @@
 // ==UserScript==
 // @name         dataTable on EmpregadorWEB
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Active dataTable on Empregador WEB (MTE website)
 // @author       pereiratiaggo
-// @match        https://sd.maisemprego.mte.gov.br/sdweb/empregadorweb/restrito/requerimentoemergencial/consultar.jsf
-// @match        https://sd.maisemprego.mte.gov.br/sdweb/empregadorweb/restrito/selecionar_empresa.jsf
+// @match        http://sd.mte.gov.br/*
+// @match        https://sd.mte.gov.br/*
+// @match        http://sd.maisemprego.mte.gov.br/*
+// @match        https://sd.maisemprego.mte.gov.br/*
 // @grant        none
 // @require      https://code.jquery.com/jquery-3.5.1.js
 // @require      http://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js
@@ -13,17 +15,19 @@
 // ==/UserScript==
 
 (function() {
+    //Table Empregados
     $('#ResultadoPesqRequerimentoEmergencial table').dataTable({
         "order":     [[ 0, "desc" ],[ 1, "asc" ]],
         "paging":    false,
         "info":      false,
         "searching": false,
     });
-    $('table#listaProcuracaoPJSelecionarTable').dataTable({
+
+    //Table Empresa
+    $('table#listaProcuracaoPJSelecionarTable, table#empresasBusca').dataTable({
         "order":     [[ 2, "asc" ]],
         "paging":    false,
         "info":      false,
         "searching": false,
     });
 })();
-
