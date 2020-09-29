fa269b9c19f16807932594a09ce5548825ba5472
Tiago Periera <pereiratiaggo@gmail.com>
2020-09-29 09:09:51 -0300

    Update autoClickOnEmpresaFacil.user.js

--- autoClickOnEmpresaFacil.user.js
+++ autoClickOnEmpresaFacil.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         autoClickOnEmpresaFacil
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!
-// @author       You
+// @version      1.0
+// @description  Clica de maneira automatica nos botões "exibir conteudo" após consulta de um procolo no EMPRESA FACIL
+// @author       pereiratiaggo
 // @match        https://www.empresafacil.pr.gov.br/sigfacil/processo/acompanhar/co_protocolo/PRP2047947317
 // @grant        none
 // ==/UserScript==
