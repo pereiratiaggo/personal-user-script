15bfcb293748724b93f629a9813d85f6cb144653
pereiratiaggo <pereirataggo@gmail.com>
2020-09-30 16:01:30 -0300

    update autoClickOnEmpresaFacil

--- autoClickOnEmpresaFacil.user.js
+++ autoClickOnEmpresaFacil.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         autoClickOnEmpresaFacil
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Clica de maneira automatica nos botões "exibir conteudo" após consulta de um procolo no EMPRESA FACIL
 // @author       pereiratiaggo
 // @match        https://www.empresafacil.pr.gov.br/sigfacil/processo/acompanhar/co_protocolo/*
@@ -12,4 +12,9 @@
     'use strict';
 
     $('a.bt-saiba.refresh-box').click();
+
+    var delay = 1000 * 60 * 15; //ms * seg * min -> 1000 * 60 * 15 = 900000ms = 15min
+    setTimeout(function () {
+        location.reload();
+    }, delay);
 })();
