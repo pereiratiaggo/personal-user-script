e38a8f4f7340482e57be0fb099b265ef113c372a
Tiago Pereira <pereiratiaggo@gmail.com>
2024-02-29 10:31:19 -0300

    sPE_FGTSDigital_Autopreencher

---
+++ sPE_FGTSDigital_Autopreenche_alguns_dados.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         sPE_FGTSDigital_Autopreenche_alguns_dados
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Este script serve para preencher alguns dados e facilitar na hora de gerar a procuração
+// @author       pereiratiaggo
+// @match        https://spe.sistema.gov.br/procuracao/criar
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=gov.br
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    let inputs;
+
+    setTimeout(() => {
+        inputs = document.getElementsByTagName("input");
+        inputs[1].value = inputs[0].value;
+    }, 1000);
+
+    setTimeout(() => {
+        inputs[3].click();
+        inputs[4].value = "" //CNPJ "00.000.000/0000-00"
+        inputs[6].value = "" //email email@email.com
+        inputs[7].value = inputs[6].value;
+    }, 1000);
+})();
